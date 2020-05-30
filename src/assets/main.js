c736a4604fcff3d413a53876147c703f50e15d9d
Ben Green <ben@bjgreen.co.uk>
2020-05-30 11:05:00 +0100

    Commented and added service worker

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,3 +1,4 @@
+// This code will fetch and load the service worker from sw.js that is located at the scope of the website (root).
 if (!navigator.serviceWorker.controller) {
 	navigator.serviceWorker.register("/sw.js").then(function(reg) {
 		console.log("Service worker has been registered for scope: " + reg.scope);
